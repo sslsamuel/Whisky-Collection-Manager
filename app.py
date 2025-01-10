d96a94b035503ba06113c5428440e8cf82e218bd
Samuel Gagatek <samuelgagatek@gmail.com>
2025-01-10 15:15:41 +0000

    added edit rating function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from whisky_manager import WhiskyManager, User, Bottle
 
 app = Flask(__name__)
@@ -22,6 +22,7 @@ def search():
     source = request.args.get('source', None)
     if source == None:
         results = WhiskyManager.search_bottles(query)
+
         return render_template('search.html', results=results, query=query)
     
     elif source == 'my_collection':
@@ -100,12 +101,19 @@ def add_to_collection(bottle_id):
 
     if user:
         bottle = Bottle.get_bottle_by_id(int(bottle_id))
-        if bottle:
+        if not any (b['id'] == bottle['id'] for b in user['collection']):
             User.amend_collection(user,bottle, True)
+            flash_message = f'{bottle['name']} added to your collection.'
+            message_type = 'success'
+        else:
+            flash_message = f'{bottle['name']} is already in your collection.'
+            message_type = 'error'
+
+    # Redirect back to the search page via AJAX
+    return jsonify({'message': flash_message, 'type': message_type})
+            
+
     
-    # Redirect back to the search page with the query parameter
-    query = request.args.get('search', "")
-    return redirect(url_for('search', search=query))
 
 
 @app.route('/remove_from_collection/<int:bottle_id>', methods=['POST'])
@@ -135,6 +143,41 @@ def my_collection():
 
     
     return redirect(url_for('search', source = 'my_collection'))
+
+@app.route('/edit_note/<int:bottle_id>', methods=['POST'])
+def edit_note(bottle_id):
+    if not is_logged_in():  # If the user is not logged in, redirect to login page
+        return redirect(url_for('login'))
+
+    note = request.form.get('note')  # Get the note from the form
+
+    username = session['username']
+    users = User.load_users()
+    user = next((u for u in users if u['username'] == username), None) # dictionary object
+
+    bottle = Bottle.get_bottle_by_id(int(bottle_id))
+
+    User.amend_collection(user, bottle, note)  # Add the note to the bottle
+
+    flash(f'Note edited successfully for {bottle['name']}!', 'success')
+    return redirect(url_for('my_collection'))
+
+@app.route('/edit_rating/<int:bottle_id>', methods=['POST'])
+def edit_rating(bottle_id):
+
+    rating = request.form['rating']
+
+    username = session['username']
+    users = User.load_users()
+    user = next((u for u in users if u['username'] == username), None) # dictionary object
+
+    bottle = Bottle.get_bottle_by_id(int(bottle_id))
+
+    User.amend_collection(user, bottle, int(rating))  # Edit the rating of the bottle
+    
+    flash(f'Rating edited successfully to {rating} stars for {bottle['name']}!', 'success')
+    return redirect(url_for('my_collection'))
+
 
 
 
